482469e28d1edc597bd56ddb615f50a6983fa653
Manar Jber <mohamadmanar960@gmail.com>
2023-11-14 17:52:37 +0200

    final edit

--- catalog-server/routes.js
+++ catalog-server/routes.js
@@ -4,5 +4,6 @@ const implementation = require('./implementation');
 
 router.get('/info/:item_number', implementation.info);
 router.get('/search/:topic', implementation.searchBooks);
+router.post('/buy/:book_id', implementation.purchase);
 
 module.exports = router;
--- order-server/implementation.js
+++ order-server/implementation.js
@@ -1,29 +1,12 @@
-const db = require('./db');
+const catalogServiceUrl = 'http://localhost:3004';
 
 exports.purchase = async (req, res) => {
-  const bookId = req.params.book_id;
-
-  db.serialize(() => {
-    // Check if the book is in stock
-    db.get('SELECT stock FROM books WHERE id = ?', [bookId], (err, book) => {
-      if (err) {
-        console.error(err.message);
-        res.status(500).json({ error: 'Failed to check stock availability.' });
-      } else if (!book) {
-        res.status(404).json({ error: 'Book not found.' });
-      } else if (book.stock <= 0) {
-        res.status(400).json({ error: 'Book is out of stock.' });
-      } else {
-        // Update stock and perform the purchase
-        db.run('UPDATE books SET stock = stock - 1 WHERE id = ?', [bookId], (updateErr) => {
-          if (updateErr) {
-            console.error(updateErr.message);
-            res.status(500).json({ error: 'Failed to update book stock.' });
-          } else {
-            res.json({ message: 'Book purchase successful' });
-          }
-        });
-      }
-    });
-  });
+  const { book_id } = req.params;
+  try {
+    const response = await axios.get(`${catalogServiceUrl}/buy/${book_id}`);
+  const data = response.data;
+  res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to process the purchase.' });
+  }
 }
