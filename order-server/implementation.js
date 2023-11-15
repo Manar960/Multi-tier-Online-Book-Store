e63a173db04249a53be4e51fc32e2327e4e65095
Rahaf Nasrallah <98306265+rahafnasralla@users.noreply.github.com>
2023-11-15 20:36:40 +0200

    Update implementation.js

--- order-server/implementation.js
+++ order-server/implementation.js
@@ -1,12 +1,26 @@
+const axios = require('axios');
+
 const catalogServiceUrl = 'http://localhost:3004';
 
 exports.purchase = async (req, res) => {
-  const { book_id } = req.params;
+  const bookId = req.params.book_id;
+
   try {
-    const response = await axios.get(`${catalogServiceUrl}/buy/${book_id}`);
-  const data = response.data;
-  res.json(data);
+    const response = await axios.get(`${catalogServiceUrl}/info/${bookId}`);
+    const book = response.data.item;
+
+    if (!book) {
+      res.status(404).json({ error: 'Book not found.' });
+    } else if (book.quantity <= 0) {
+      res.status(400).json({ error: 'Book is out of stock.' });
+    } else {
+      const updatePayload = { quantity: book.quantity - 1 };
+      await axios.put(`${catalogServiceUrl}/update/${bookId}`, updatePayload);
+
+      res.json({ message: 'Book purchase successful' });
+    }
   } catch (error) {
-    res.status(500).json({ error: 'Failed to process the purchase.' });
+    console.error(error);
+    res.status(500).json({ error: 'Failed to check stock availability or update book stock.' });
   }
-}
+};
