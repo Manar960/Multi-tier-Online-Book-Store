2c8ef3a32fb6e5336f3a69d58a8c6a773f8fc19b
Rahaf Nasrallah <nasrallahrahaf7@gmail.com>
2023-11-23 12:12:16 +0200

    -

--- catalog-server/implementation.js
+++ catalog-server/implementation.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-<<<<<<< HEAD
 const csvFilePath = 'books.csv';
 
 exports.info = async (req, res) => {
@@ -13,44 +12,6 @@ exports.info = async (req, res) => {
     .on('data', (row) => {
       if (row.id === item_number) {
         foundBook.push(row);
-=======
-  exports.searchBooks = async (req, res) => {
-    const { topic } = req.params;
-    db.all('SELECT * FROM books WHERE topic = ?', topic, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: 'Failed to retrieve data from the catalog.' });
-        return;
-      }
-      res.json({ books: rows });
-    });
-  };
-
-
-exports.update = async (req, res) => {
-    const { item_number } = req.params;
-    const { cost, quantity } = req.body;
-  
-    if (!cost && !quantity) {
-      res.status(400).json({ error: 'Missing parameters for update.' });
-      return;
-    }
-  
-    const updateQuery = 'UPDATE books SET ' +
-      (cost ? 'cost = ? ' : '') +
-      (cost && quantity ? ', ' : '') +
-      (quantity ? 'quantity = ? ' : '') +
-      'WHERE id = ?';
-  
-    const params = [];
-    if (cost) params.push(cost);
-    if (quantity) params.push(quantity);
-    params.push(item_number);
-  
-    db.run(updateQuery, params, function (err) {
-      if (err) {
-        res.status(500).json({ error: 'Failed to update data in the catalog.' });
-        return;
->>>>>>> 386e0bd0e03ee3467d4091428ceead3e5dea1a3e
       }
     })
     .on('end', () => {
@@ -60,7 +21,6 @@ exports.update = async (req, res) => {
         res.json({ book: foundBook[0] });
       }
     });
-<<<<<<< HEAD
 };
 
 exports.searchBooks = async (req, res) => {
@@ -82,6 +42,7 @@ exports.searchBooks = async (req, res) => {
 exports.update = async (req, res) => {
   const { item_number } = req.params;
   const { cost, quantity } = req.body;
+
   if (!cost && !quantity) {
     res.status(400).json({ error: 'Missing parameters for update.' });
     return;
@@ -99,7 +60,7 @@ exports.update = async (req, res) => {
     })
     .on('end', () => {
       const writeStream = fs.createWriteStream(csvFilePath);
-      writeStream.write('id,title,author,topic,cost,quantity\n'); // CSV header
+      writeStream.write('id,title,topic,cost,quantity\n'); // CSV header
       updatedBooks.forEach((row) => {
         writeStream.write(`${row.id},${row.title},${row.author},${row.topic},${row.cost},${row.quantity}\n`);
       });
@@ -108,7 +69,3 @@ exports.update = async (req, res) => {
       res.json({ status: 'success' });
     });
 };
-=======
-  };
-  
->>>>>>> 386e0bd0e03ee3467d4091428ceead3e5dea1a3e
--- order-server/implementation.js
+++ order-server/implementation.js
@@ -3,34 +3,21 @@ const axios = require('axios');
 const catalogServiceUrl = 'http://localhost:3004';
 
 exports.purchase = async (req, res) => {
-  const item_number = req.params.book_id;
+  const bookId = req.params.book_id;
 
   try {
-<<<<<<< HEAD
     // Check if the book is in stock
-    const response = await axios.get(`${catalogServiceUrl}/info/${item_number}`);
-    const book = response.data.book;
-=======
     const response = await axios.get(`${catalogServiceUrl}/info/${bookId}`);
     const book = response.data.item;
-
->>>>>>> 386e0bd0e03ee3467d4091428ceead3e5dea1a3e
     if (!book) {
       res.status(404).json({ error: 'Book not found.' });
     } 
     else if (book.quantity <= 0) {
       res.status(400).json({ error: 'Book is out of stock.' });
-<<<<<<< HEAD
     } 
-    else {
-      // Update stock and perform the purchase
-      const updatePayload = { cost: book.cost,  quantity: book.quantity - 1 };
-      await axios.put(`${catalogServiceUrl}/update/${item_number}`, updatePayload);
-=======
-    } else {
+else {
       const updatePayload = { quantity: book.quantity - 1 };
       await axios.put(`${catalogServiceUrl}/update/${bookId}`, updatePayload);
->>>>>>> 386e0bd0e03ee3467d4091428ceead3e5dea1a3e
 
       res.json({ message: 'Book purchase successful' });
     }
